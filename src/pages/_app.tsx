9af7b0e6385a664924b746e1f150561c4679cb93
JOCECODE <ulah57@gmail.com>
2023-04-22 12:30:18 -0700

    added middleware, signin and sign out button;

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,12 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+import { ClerkProvider } from '@clerk/nextjs';
+import type { AppProps } from 'next/app'
 
-import { api } from "~/utils/api";
-
-import "~/styles/globals.css";
-
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+function MyApp({ Component, pageProps }) {
   return (
-    <SessionProvider session={session}>
+    <ClerkProvider {...pageProps} >
       <Component {...pageProps} />
-    </SessionProvider>
+    </ClerkProvider>
   );
-};
+}
 
-export default api.withTRPC(MyApp);
+export default MyApp;
